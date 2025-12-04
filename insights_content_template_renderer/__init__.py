27a470c30a69de52ff9d81be11edf6b72976a2ae
Jiří Papoušek <jipapous@redhat.com>
2025-12-04 15:31:39 +0100

    Remove unnecessary comment

--- insights_content_template_renderer/__init__.py
+++
@@ -1,3 +0,0 @@
-# CVE-2024-28397 patch removed as we've migrated from js2py to pythonmonkey
-# pythonmonkey uses SpiderMonkey (Mozilla's JavaScript engine) which is actively maintained
-# and doesn't have the same security vulnerabilities as js2py
